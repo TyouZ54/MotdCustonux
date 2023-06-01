dfe933165fb30503688010da7524f5a85f0d2711
TyouZ <34603107+TyouZ54@users.noreply.github.com>
2023-06-01 13:57:57 +0200

    Update InstallMotd.sh

--- InstallMotd.sh
+++ InstallMotd.sh
@@ -5,7 +5,10 @@ mv /etc/update-motd.d/* /etc/update-motd.d/old/
 
 rm /etc/motd
 
-wget https://raw.githubusercontent.com/TyouZ54/MotdCustonux/main/Content -O /etc/update-motd.d/00-motd
+wget https://raw.githubusercontent.com/TyouZ54/MotdCustonux/main/Content1 -O /etc/update-motd.d/00-motd
+wget https://raw.githubusercontent.com/TyouZ54/MotdCustonux/main/Content2 -O /etc/update-motd.d/10-motd
 
 chmod +x /etc/update-motd.d/00-motd
+chmod +x /etc/update-motd.d/10-motd
 chmod 755 /etc/update-motd.d/00-motd
+chmod 755 /etc/update-motd.d/10-motd
